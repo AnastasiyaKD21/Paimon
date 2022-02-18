68b11f2589ac3f76490de00b962a8522981ce87e
unknown <anastasiyakd21@gmail.com>
2022-02-18 17:41:11 +0700

    Update

---
+++ Paimon's Delicious Adventures!/Assets/Mora/Script/Mora.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mora : MonoBehaviour
+{
+    public float speed = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 40 * Time.deltaTime * speed);
+    }
+}
